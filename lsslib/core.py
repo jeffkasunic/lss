ff793ff25cb2c4de5397df75590c0b0e71aa14f2
Jeff Kasunic <github.kwb8u@dev.jeffkasunic.com>
2026-05-02 22:05:55 -0700

    lsslib core functions - get_unpadded_frames, get_padding

--- lsslib/core.py
+++ lsslib/core.py
@@ -1,16 +1,42 @@
-# Seperate sequences into groups.
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def get_identical_string():
     """
     Find left justified identical, matching chars. Used to find grouping.
     It compares the current item to the previous item in the array.
     """
-    
-# Display sequence groups in compact representation
-def get_padding():
+
+
+def get_unpadded_framenumber(filename):
+    """
+    Get the current frame number with padding removed.
+    """
+    frame_number = int(filename.split('.')[-1])
+    logger.info('Unpadded frame number: ' + str(frame_number))
+
+    return frame_number
+
+
+def get_padding(filename):
     """
     Get the padding number in an array of integers.
     """
-    return None
+    frame_number = filename.split('.')[-1]
+    number_length = len(filename.split('.')[-1])
+
+    logger.info('Frame number: ' + str(frame_number))
+    logger.info('Frame number length: ' + str(number_length))
+
+    if number_length == 1 or frame_number == "0":
+        return 0  #Zero-padded
+    elif number_length > 1 and not frame_number.startswith('0'):
+        return 0  #Zero-padded
+    else:
+        return number_length
+
 
 def get_startframe():
     """
@@ -18,11 +44,13 @@ def get_startframe():
     """
     return None
 
+
 def get_endframe():
     """
     Get the max value in an array of integers.
     """
     return None
+
 
 def get_missing_frame():
     """
@@ -30,11 +58,13 @@ def get_missing_frame():
     """
     return None
 
+
 def get_stepnumber():
     """
     Get the step number in an array of integers.
     """
     return None
+
 
 def is_contiguous():
     """
@@ -42,9 +72,9 @@ def is_contiguous():
     """
     return None
 
+
 def remove_padding():
     """
     Remove the padding chars in front of an array of integers.
     """
     return None
-
