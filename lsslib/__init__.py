0b1c618e8672f7229a53a2c69f020f5bf54d748a
Jeff Kasunic <github.kwb8u@dev.jeffkasunic.com>
2026-05-23 10:07:13 -0700

    Sequence create cleanup

--- lsslib/__init__.py
+++ lsslib/__init__.py
@@ -3,7 +3,6 @@ __version__ = "0.1.0"
 
 import logging
 
-from .sequence import seq_create, seq_rename
 from .core import (get_padding, get_missing_frames, is_contiguous, is_mixed_padding, get_step_size,
                    get_endframe, get_startframe, find_key, group_keys)
 
--- lsslib/sequence.py
+++ lsslib/sequence.py
@@ -4,18 +4,10 @@ from pathlib import Path
 logger = logging.getLogger(__name__)
 
 
-def seq_create(filename: str, range_start: int = 0, range_end: int = 20, padding: int = 4, step: int = 1,
-               path: str = "./"):
+def create(stem: str, start_frame: int = 0, end_frame: int = 20, padding: int = 4, step: int = 1, path: str = "./") -> None:
     """
     Create a sequence of files to be used as input to lsslib.
     """
-    for i in range(range_start, range_end + 1, step):
-        Path(path + filename + "." + str.rjust(str(i), padding, "0") + ".exr").touch()
-    logger.info(f"  Created {filename}")
-
-
-def seq_rename():
-    """
-    Rename a sequence of files.
-    """
-    return None
+    for i in range(start_frame, end_frame + 1, step):
+        Path(path + stem + "." + str(i).zfill(padding) + ".exr").touch()
+    logger.info(f"Created {stem}")
